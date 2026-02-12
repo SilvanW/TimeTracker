22b2dc26bb172aaa23cba44b57a794114e5791ca
SilvanW <silvan.wiedmer@bluewin.ch>
2026-02-12 18:46:33 +0100

    add cors middleware to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .database.db import create_db_and_tables
 from .routers import projects, time
 from .routers import analytics
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(projects.router)
 app.include_router(time.router)
